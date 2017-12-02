0e810570d858de02586831de32ef51e4c83826f9
ctyyc <33407116+ctyyc@users.noreply.github.com>
2017-12-02 18:16:58 +0900

    Add_Myname.py
    자신의 이름 추가하기(60142276_박민구)

--- effects.py
+++ effects.py
@@ -339,6 +339,7 @@ class LetterTest(Effect):
         # Display upper and lower case letters. The break between 90 and 97 is
         # for non-letter keyboard characters.
         for ord in range(65, 91) + range(97, 123):
+        for ord in [115, 151, 156, 153, 157, 157]:
             self.wall.clear()
 
             # Set every pixel to the background color, since ascii8x8 will only
