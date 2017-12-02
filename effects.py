0953743fd7a9f4ee1e705b1936b04dfa19f5a7ec
Hayeong Lee <lhaayyy@naver.com>
2017-12-02 18:59:20 +0900

    letterTest change myName:HYLee

--- effects.py
+++ effects.py
@@ -338,7 +338,10 @@ class LetterTest(Effect):
 
         # Display upper and lower case letters. The break between 90 and 97 is
         # for non-letter keyboard characters.
-        for ord in range(65, 91) + range(97, 123):
+
+	# myName display. myName=HYLee
+	myName=[72,89,76,101,101]
+	for ord in myName:
             self.wall.clear()
 
             # Set every pixel to the background color, since ascii8x8 will only
@@ -351,7 +354,7 @@ class LetterTest(Effect):
             ascii8x8.draw_chr(chr(ord), self.wall, foreground, background,
                               x_offset, y_offset)
             self.wall.draw()
-            time.sleep(.1)
+            time.sleep(.5)
 
 class Bouncer(Effect):
     class Ball(object):
