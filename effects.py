d9cccf2b260d4ff6cef9c9e42c00175787e232fd
kyungchan <kkc8983@gmail.com>
2017-12-02 17:08:57 +0900

    Update effects.py

--- effects.py
+++ effects.py
@@ -338,7 +338,7 @@ class LetterTest(Effect):
 
         # Display upper and lower case letters. The break between 90 and 97 is
         # for non-letter keyboard characters.
-        for ord in range(65, 91) + range(97, 123):
+        for ord in [75, 121, 117, 110, 103, 67, 104, 97, 110]:
             self.wall.clear()
 
             # Set every pixel to the background color, since ascii8x8 will only
