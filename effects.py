c6a2111977ff3ded4716d7cd8dfcf7849dd57360
yuyaebean <yuyaebean@naver.com>
2017-12-02 21:56:57 +0900

    Change LetterTest string to 'YebinYu'

--- effects.py
+++ effects.py
@@ -338,7 +338,7 @@ class LetterTest(Effect):
 
         # Display upper and lower case letters. The break between 90 and 97 is
         # for non-letter keyboard characters.
-        for ord in range(65, 91) + range(97, 123):
+        for ord in [89, 101, 98, 105, 110, 89, 117]:
             self.wall.clear()
 
             # Set every pixel to the background color, since ascii8x8 will only
@@ -351,7 +351,7 @@ class LetterTest(Effect):
             ascii8x8.draw_chr(chr(ord), self.wall, foreground, background,
                               x_offset, y_offset)
             self.wall.draw()
-            time.sleep(.1)
+            time.sleep(1)
 
 class Bouncer(Effect):
     class Ball(object):
